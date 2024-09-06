8b94b2cf9b228e75d0d15164e3443951c3af06e6
gillcowen <gill.cowen@curtin.edu.au>
2024-09-06 11:50:54 +0800

    Add welcome message and collect the filename

--- goostats.sh
+++ goostats.sh
@@ -1,3 +1,6 @@
 # Load given file
 # Compute the min/max/range of values in a file
-
+fname=$1
+echo "working with file"
+echo "welcome to Nell's stats script"
+#Compute the min//max/range of values in a file
